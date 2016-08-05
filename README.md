fc31e4f0c02c8ae8b8700a906696736c52720287
georapbox <georapbox@gmail.com>
2016-08-05 18:47:56 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,131 @@
+<a name="module_eventDispatcher"></a>
+
+## eventDispatcher
+JavaScript events for custom objects
+
+**See**: original project at `https://github.com/mrdoob/eventdispatcher.js`  
+**Example**  
+```js
+var car = {
+  start: function (afterStartCallback) {
+    // Dispatch `start` event.
+    this.dispatchEvent({type: 'start', message: 'Start engines'});
+    afterStartCallback && afterStartCallback(this);
+  },
+  stop: function () {
+    // Dispatch `stop` event.
+    this.dispatchEvent({type: 'stop', message: 'Stop engines'});
+  }
+};
+
+function startEngines(event) {
+  console.log(event.message);
+}
+
+function stopEngines(event) {
+  console.log(event.message);
+}
+
+// Apply `eventDispatcher` methods to the prototype of the event target.
+eventDispatcher.apply(Object.getPrototypeOf(car));
+
+// Register event listeners to event target.
+car
+  .addEventListener('start', startEngines)
+  .addEventListener('afterStart', startEngines)
+  .addEventListener('stop', stopEngines);
+
+// `start` event fires first, then `afterStart` and finally
+// after two seconds `stop` events fires.
+
+car.start(function (self) {
+  // Dispatch `afterStart` event.
+  self.dispatchEvent({type: 'afterStart', message: 'Car has started'});
+});
+
+console.log(car.hasEventListener('start', startEngines));      // -> true
+console.log(car.hasEventListener('afterStart', startEngines)); // -> true
+console.log(car.hasEventListener('stop', startEngines));       // -> false
+console.log(car.hasEventListener('stop', stopEngines));        // -> true
+
+setTimeout(function () {
+  car.stop();
+
+  // Remove all event listeners from event target.
+  car
+    .removeEventListener('start', startEngines)
+    .removeEventListener('afterStart', startEngines)
+    .removeEventListener('stop', stopEngines);
+}, 2000);
+```
+
+* [eventDispatcher](#module_eventDispatcher)
+    * [~addEventListener(type, listener)](#module_eventDispatcher..addEventListener) ⇒ <code>Object</code>
+    * [~hasEventListener(type, listener)](#module_eventDispatcher..hasEventListener) ⇒ <code>Boolean</code>
+    * [~removeEventListener(type, listener)](#module_eventDispatcher..removeEventListener) ⇒ <code>Object</code>
+    * [~dispatchEvent(event)](#module_eventDispatcher..dispatchEvent) ⇒ <code>Object</code>
+    * [~apply(object)](#module_eventDispatcher..apply) ⇒ <code>Object</code>
+
+<a name="module_eventDispatcher..addEventListener"></a>
+
+### eventDispatcher~addEventListener(type, listener) ⇒ <code>Object</code>
+Registers the specified `listener` on the event target it's called on.
+
+**Kind**: inner method of <code>[eventDispatcher](#module_eventDispatcher)</code>  
+**Returns**: <code>Object</code> - The `eventDispatcher` object.  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| type | <code>String</code> | A string representing the event type to listen for. |
+| listener | <code>function</code> | A function to be executed when an event of the specified `type` occurs. |
+
+<a name="module_eventDispatcher..hasEventListener"></a>
+
+### eventDispatcher~hasEventListener(type, listener) ⇒ <code>Boolean</code>
+Checks if the target object has a `listener` registered on for specific event `type`..
+
+**Kind**: inner method of <code>[eventDispatcher](#module_eventDispatcher)</code>  
+**Returns**: <code>Boolean</code> - True if target object has `listener` registered for specific event `type`; otherwise false.  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| type | <code>String</code> | A string representing the event type. |
+| listener | <code>function</code> | The event listener to check if registered for the specified event `type`. |
+
+<a name="module_eventDispatcher..removeEventListener"></a>
+
+### eventDispatcher~removeEventListener(type, listener) ⇒ <code>Object</code>
+Removes the previously registered event `listener` from the event target.
+
+**Kind**: inner method of <code>[eventDispatcher](#module_eventDispatcher)</code>  
+**Returns**: <code>Object</code> - The `eventDispatcher` object.  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| type | <code>String</code> | A string representing the event type to remove. |
+| listener | <code>function</code> | The event listener function to remove from the event target. |
+
+<a name="module_eventDispatcher..dispatchEvent"></a>
+
+### eventDispatcher~dispatchEvent(event) ⇒ <code>Object</code>
+Dispatches an event at the specified event target.
+
+**Kind**: inner method of <code>[eventDispatcher](#module_eventDispatcher)</code>  
+**Returns**: <code>Object</code> - The `eventDispatcher` object.  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| event | <code>Object</code> | The event object to be dispatched. |
+
+<a name="module_eventDispatcher..apply"></a>
+
+### eventDispatcher~apply(object) ⇒ <code>Object</code>
+Applies the `eventDispatcher` prototype methods to the event target.
+
+**Kind**: inner method of <code>[eventDispatcher](#module_eventDispatcher)</code>  
+**Returns**: <code>Object</code> - The `eventDispatcher` object.  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| object | <code>Object</code> | The event target object. |
+
---
+++ src/eventDispatcher.js
@@ -0,0 +1,207 @@
+/**
+ * JavaScript events for custom objects
+ *
+ * @module eventDispatcher
+ * @see original project at `https://github.com/mrdoob/eventdispatcher.js`
+ * @example
+ *
+ * var car = {
+ *   start: function (afterStartCallback) {
+ *     // Dispatch `start` event.
+ *     this.dispatchEvent({type: 'start', message: 'Start engines'});
+ *     afterStartCallback && afterStartCallback(this);
+ *   },
+ *   stop: function () {
+ *     // Dispatch `stop` event.
+ *     this.dispatchEvent({type: 'stop', message: 'Stop engines'});
+ *   }
+ * };
+ *
+ * function startEngines(event) {
+ *   console.log(event.message);
+ * }
+ *
+ * function stopEngines(event) {
+ *   console.log(event.message);
+ * }
+ *
+ * // Apply `eventDispatcher` methods to the prototype of the event target.
+ * eventDispatcher.apply(Object.getPrototypeOf(car));
+ *
+ * // Register event listeners to event target.
+ * car
+ *   .addEventListener('start', startEngines)
+ *   .addEventListener('afterStart', startEngines)
+ *   .addEventListener('stop', stopEngines);
+ *
+ * // `start` event fires first, then `afterStart` and finally
+ * // after two seconds `stop` events fires.
+ *
+ * car.start(function (self) {
+ *   // Dispatch `afterStart` event.
+ *   self.dispatchEvent({type: 'afterStart', message: 'Car has started'});
+ * });
+ *
+ * console.log(car.hasEventListener('start', startEngines));      // -> true
+ * console.log(car.hasEventListener('afterStart', startEngines)); // -> true
+ * console.log(car.hasEventListener('stop', startEngines));       // -> false
+ * console.log(car.hasEventListener('stop', stopEngines));        // -> true
+ *
+ * setTimeout(function () {
+ *   car.stop();
+ *
+ *   // Remove all event listeners from event target.
+ *   car
+ *     .removeEventListener('start', startEngines)
+ *     .removeEventListener('afterStart', startEngines)
+ *     .removeEventListener('stop', stopEngines);
+ * }, 2000);
+ */
+(function (name, context, definition) {
+  'use strict';
+
+  if (typeof define === 'function' && define.amd) {
+    define(definition);
+  } else if (typeof module !== 'undefined' && module.exports) {
+    module.exports = definition();
+  } else {
+    context[name] = definition(name, context);
+  }
+}('eventDispatcher', this, function () {
+  'use strict';
+
+  /** @lends eventDispatcher */
+  var eventDispatcherPrototype = {
+    /**
+     * Registers the specified `listener` on the event target it's called on.
+     *
+     * @function addEventListener
+     * @param {String} type A string representing the event type to listen for.
+     * @param {function} listener A function to be executed when an event of the specified `type` occurs.
+     * @return {Object} The `eventDispatcher` object.
+     */
+    addEventListener: function (type, listener) {
+      var listeners;
+
+      if (this._listeners === undefined) {
+        this._listeners = {};
+      }
+
+      listeners = this._listeners;
+
+      if (listeners[type] === undefined) {
+        listeners[type] = [];
+      }
+
+      if (listeners[type].indexOf(listener) === - 1) {
+        listeners[type].push(listener);
+      }
+
+      return this;
+    },
+
+    /**
+     * Checks if the target object has a `listener` registered on for specific event `type`..
+     *
+     * @function hasEventListener
+     * @param {String} type A string representing the event type.
+     * @param {function} listener The event listener to check if registered for the specified event `type`.
+     * @return {Boolean} True if target object has `listener` registered for specific event `type`; otherwise false.
+     */
+    hasEventListener: function (type, listener) {
+      var listeners;
+
+      if (this._listeners === undefined) {
+        return false;
+      }
+
+      listeners = this._listeners;
+
+      if (listeners[type] !== undefined && listeners[type].indexOf(listener) !== - 1) {
+        return true;
+      }
+
+      return false;
+    },
+
+    /**
+     * Removes the previously registered event `listener` from the event target.
+     *
+     * @function removeEventListener
+     * @param {String} type A string representing the event type to remove.
+     * @param {function} listener The event listener function to remove from the event target.
+     * @return {Object} The `eventDispatcher` object.
+     */
+    removeEventListener: function (type, listener) {
+      var listeners, listenerArray, index;
+
+      if (this._listeners === undefined) {
+        return;
+      }
+
+      listeners = this._listeners;
+      listenerArray = listeners[type];
+
+      if (listenerArray !== undefined) {
+        index = listenerArray.indexOf(listener);
+
+        if (index !== - 1) {
+          listenerArray.splice(index, 1);
+        }
+      }
+
+      return this;
+    },
+
+    /**
+     * Dispatches an event at the specified event target.
+     *
+     * @function dispatchEvent
+     * @param {Object} event The event object to be dispatched.
+     * @return {Object} The `eventDispatcher` object.
+     */
+    dispatchEvent: function (event) {
+      var listeners, listenerArray, i, length;
+
+      if (this._listeners === undefined) {
+        return;
+      }
+
+      listeners = this._listeners;
+      listenerArray = listeners[event.type];
+
+      if (listenerArray !== undefined) {
+        event.target = this;
+
+        length = listenerArray.length;
+
+        for (i = 0; i < length; i += 1) {
+          listenerArray[i].call(this, event);
+        }
+      }
+
+      return this;
+    }
+  };
+
+  var eventDispatcher = Object.create(eventDispatcherPrototype, {
+    apply: {
+      /**
+       * Applies the `eventDispatcher` prototype methods to the event target.
+       *
+       * @function apply
+       * @param {Object} object The event target object.
+       * @return {Object} The `eventDispatcher` object.
+       */
+      value: function applyEventDispatcher(object) {
+        object.addEventListener = eventDispatcherPrototype.addEventListener;
+        object.hasEventListener = eventDispatcherPrototype.hasEventListener;
+        object.removeEventListener = eventDispatcherPrototype.removeEventListener;
+        object.dispatchEvent = eventDispatcherPrototype.dispatchEvent;
+        return this;
+      }
+    }
+  });
+
+  return eventDispatcher;
+}));
