1648231b93185bfca60f773b58ceaa5e93b868f7
georapbox <georapbox@gmail.com>
2016-08-07 18:45:04 +0300

    update example

--- examples/main.js
+++ examples/main.js
@@ -1,56 +1,57 @@
-/* jshint ignore:start */
-// jscs: disable
+/* eslint-disable no-console */
 
-'use strict';
+(function () {
+  'use strict';
 
-var car = {
-  start: function (afterStartCallback) {
-    // Dispatch `start` event.
-    this.dispatchEvent({type: 'start', message: 'Start engines'});
-    afterStartCallback && afterStartCallback(this);
-  },
-  stop: function () {
-    // Dispatch `stop` event.
-    this.dispatchEvent({type: 'stop', message: 'Stop engines'});
+  var car = {
+    start: function (afterStartCallback) {
+      // Dispatch `start` event.
+      this.dispatchEvent({type: 'start', message: 'Start engines'});
+      afterStartCallback && afterStartCallback(this);
+    },
+    stop: function () {
+      // Dispatch `stop` event.
+      this.dispatchEvent({type: 'stop', message: 'Stop engines'});
+    }
+  };
+
+  function startEngines(event) {
+    console.log(event.message);
   }
-};
 
-function startEngines(event) {
-  console.log(event.message);
-}
+  function stopEngines(event) {
+    console.log(event.message);
+  }
 
-function stopEngines(event) {
-  console.log(event.message);
-}
+  // Apply `eventDispatcher` methods to the prototype of the event target.
+  eventDispatcher.apply(Object.getPrototypeOf(car));
 
-// Apply `eventDispatcher` methods to the prototype of the event target.
-eventDispatcher.apply(Object.getPrototypeOf(car));
-
-// Register event listeners to event target.
-car
-  .addEventListener('start', startEngines)
-  .addEventListener('afterStart', startEngines)
-  .addEventListener('stop', stopEngines);
-
-// `start` event fires first, then `afterStart` and finally
-// after two seconds `stop` events fires.
-
-car.start(function (self) {
-  // Dispatch `afterStart` event.
-  self.dispatchEvent({type: 'afterStart', message: 'Car has started'});
-});
-
-console.log(car.hasEventListener('start', startEngines));      // -> true
-console.log(car.hasEventListener('afterStart', startEngines)); // -> true
-console.log(car.hasEventListener('stop', startEngines));       // -> false
-console.log(car.hasEventListener('stop', stopEngines));        // -> true
-
-setTimeout(function () {
-  car.stop();
-
-  // Remove all event listeners from event target.
+  // Register event listeners to event target.
   car
-    .removeEventListener('start', startEngines)
-    .removeEventListener('afterStart', startEngines)
-    .removeEventListener('stop', stopEngines);
-}, 2000);
+    .addEventListener('start', startEngines)
+    .addEventListener('afterStart', startEngines)
+    .addEventListener('stop', stopEngines);
+
+  // `start` event fires first, then `afterStart` and finally
+  // after two seconds `stop` events fires.
+
+  car.start(function (self) {
+    // Dispatch `afterStart` event.
+    self.dispatchEvent({type: 'afterStart', message: 'Car has started'});
+  });
+
+  console.log(car.hasEventListener('start', startEngines));      // -> true
+  console.log(car.hasEventListener('afterStart', startEngines)); // -> true
+  console.log(car.hasEventListener('stop', startEngines));       // -> false
+  console.log(car.hasEventListener('stop', stopEngines));        // -> true
+
+  setTimeout(function () {
+    car.stop();
+
+    // Remove all event listeners from event target.
+    car
+      .removeEventListener('start', startEngines)
+      .removeEventListener('afterStart', startEngines)
+      .removeEventListener('stop', stopEngines);
+  }, 2000);
+}());
